b34cc5f666b3a6a1b50a63825268429bca8a6a09
Dane Kozak <danekozak@wirelessprv-10-192-115-152.near.illinois.edu>
2018-09-22 11:43:27 -0500

    progress on starting path strats

--- MyBot.cpp
+++ MyBot.cpp
@@ -15,6 +15,25 @@ using DeathEffects = Game_Api::DeathEffects;
 #include <iostream>
 using namespace std;
 //You may add global variables and helper functions
+vector<node_id_t> starting_path = {10,16,12,22,21,20};
+int start_path_idx = 0;
+bool bool_starting_path = 1;
+
+string attack_monster(node_id_t location, Game_Api * api){
+	Monster m = api->get_monster(location);
+	if(m._stance == "Rock") return "Paper";
+	else if(m._stance == "Paper") return "Scissors";
+	else  return "Rock"; //(m._stance == "Scissors")
+}
+
+string attack_player(node_id_t location, Game_Api * api){//counter opponent's last move
+	Player p = api->get_opponent();
+	if(p._stance == "Rock") return "Paper";
+	else if(p._stance == "Paper") return "Scissors";
+	else return "Rock"; //(p._stance == "Scissors")
+}
+
+void start_path_strats()
 
 int main() {
 	Game_Api * api;
@@ -31,10 +50,17 @@ int main() {
 			api->update(data["game_data"]);
 
 			 //YOUR CODE HERE
-				Player me = api->get_self();
-			  Player opponent = api->get_opponent();
-			  node_id_t destination_decision = me._location;
-			  string stance = "Paper";
+			 Player me = api->get_self();
+			 Player opponent = api->get_opponent();
+
+		//defaults
+			 node_id_t destination_decision = 0;
+			 string stance = "Rock";
+
+			 if(bool_starting_path){//on starting path
+
+			 }
+
 
 								api->submit_decision(destination_decision,stance); //CHANGE THIS
              		fflush(stdout);
